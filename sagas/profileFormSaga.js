799d981140ad6f3f1fc0a752bfaef92cf93f07d8
adambarghouthi <adambargh@gmail.com>
2020-10-05 11:40:32 -0400

    initial commit

---
+++ sagas/profileFormSaga.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { call, put, takeLeading } from 'redux-saga/effects'
+import {
+  actionTypes,
+  handleSuccess,
+  handleError,
+  handleChange
+} from 'actions/profileFormActions'
+
+import { handleEdit } from 'actions/userActions'
+
+function* profileFormSubmit(action) {
+  const { form } = action
+  const url = '/api/users'
+
+  let token
+
+  try {
+    token = localStorage.getItem('token')
+
+    const res = yield call(() => axios.put(url, { token, ...form }))
+
+    const { data } = res
+    if (data.status === 'error') throw data
+
+    const session = data.data
+
+    yield localStorage.setItem('token', session.token)
+    yield put(handleEdit(session.user))
+    yield put(handleSuccess('profile_form_success'))
+  } catch (error) {
+    yield put(handleError(error.message))
+  }
+}
+
+export default function* watchProfileFormSubmit() {
+  yield takeLeading(actionTypes.PROFILE_FORM_SUBMIT, profileFormSubmit)
+}
