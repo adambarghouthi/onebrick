d339150c0d849719805e5cdf7391ababc796ced6
adambarghouthi <adambargh@gmail.com>
2020-10-06 14:26:30 -0400

    add pmList and sub reducers

--- reducers/index.js
+++ reducers/index.js
@@ -3,19 +3,22 @@ import changePwdFormReducer from './changePwdFormReducer'
 import loginFormReducer from './loginFormReducer'
 import memListReducer from './memListReducer'
 import pmFormReducer from './pmFormReducer'
+import pmListReducer from './pmListReducer'
 import profileFormReducer from './profileFormReducer'
 import resetPwdFormReducer from './resetPwdFormReducer'
 import signupFormReducer from './signupFormReducer'
 import userReducer from './userReducer'
+import subReducer from './subReducer'
 
 export default combineReducers({
   changePwdForm: changePwdFormReducer,
   loginForm: loginFormReducer,
   memList: memListReducer,
   pmForm: pmFormReducer,
-  // pmList: pmListReducer,
+  pmList: pmListReducer,
   profileForm: profileFormReducer,
   resetPwdForm: resetPwdFormReducer,
   signupForm: signupFormReducer,
-  user: userReducer
+  user: userReducer,
+  sub: subReducer
 })
