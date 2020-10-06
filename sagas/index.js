1a41a87c50c2a7ef15cd31dd00bde9736db66cba
adambarghouthi <adambargh@gmail.com>
2020-10-06 14:26:53 -0400

    add watchSubFetch, watchPmListFetch, profileFormSaga, changePwdFormSaga

--- sagas/index.js
+++ sagas/index.js
@@ -1,18 +1,26 @@
 import { all, fork } from 'redux-saga/effects'
 import signupFormSaga from './signupFormSaga'
 import loginFormSaga from './loginFormSaga'
+import profileFormSaga from './profileFormSaga'
+import changePwdFormSaga from './changePwdFormSaga'
 import { watchUserFetch, watchUserLogout } from './userSaga'
 import { watchMemListFetch } from './memListSaga'
 import { watchPmFormSubscribe } from './pmFormSaga'
+import { watchSubFetch } from './subSaga'
+import { watchPmListFetch } from './pmListSaga'
 
 function* rootSaga() {
   yield all([
     fork(signupFormSaga),
     fork(loginFormSaga),
+    fork(profileFormSaga),
+    fork(changePwdFormSaga),
     fork(watchPmFormSubscribe),
     fork(watchUserFetch),
     fork(watchUserLogout),
     fork(watchMemListFetch),
+    fork(watchSubFetch),
+    fork(watchPmListFetch)
   ])
 }
 
