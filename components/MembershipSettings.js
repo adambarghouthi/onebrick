6c99fd8465160fe04840587b0979a85ce3541e90
adambarghouthi <adambargh@gmail.com>
2020-10-10 19:45:46 -0400

    initial commit

---
+++ components/MembershipSettings.js
@@ -0,0 +1,106 @@
+import React, { useEffect } from 'react'
+import { Card, Typography, Button, Row, Col, Skeleton } from 'antd'
+// import { CheckCircleOutlined, HeartTwoTone } from '@ant-design/icons'
+import useTranslation from 'lib/translations/useTranslation'
+import { languageDirection } from 'lib/translations/config'
+import { LocaleContext } from 'context/LocaleContext'
+
+const { Title, Text } = Typography
+
+const gridStyle = {
+  width: '50%',
+}
+
+const MembershipSettings = (props) => {
+  const {
+    onCancel,
+    onActivate,
+    onEdit,
+    data
+  } = props
+
+  const { t } = useTranslation()
+  const { locale } = React.useContext(LocaleContext)
+  const direction = languageDirection[locale] || 'ltr'
+
+  const subIsActive = (data.sub &&
+    data.sub.pause_collection &&
+    data.sub.pause_collection.behaviour)
+      ? true
+      : false
+
+  return (
+    <Card>
+      <Card.Grid hoverable={false} style={{ width: '100%' }}>
+        <Title className="mb-0" level={4}>
+          { t('membership_settings') }
+        </Title>
+      </Card.Grid>
+      <Card.Grid hoverable={false} style={{ width: '100%' }}>
+        <Row justify="space-between" align="middle">
+          <Col span={18}>
+            <Skeleton
+              loading={data.loading && !data.sub}
+              paragraph={{ rows: 1 }}
+              active
+            >
+              <Text>
+                {
+                  subIsActive
+                    ? t('cancel_membership')
+                    : t('activate_membership')
+                }
+              </Text>
+              <br />
+              <Text type="secondary">
+                {
+                  subIsActive
+                    ? t('cancel_membership_secondary')
+                    : t('activate_membership_secondary')
+                }
+              </Text> 
+            </Skeleton>
+          </Col>
+          <Col span={6}>
+            {
+              !data.sub
+                ? <Skeleton.Button
+                    loading={data.loading}
+                    size="large"
+                    shape="default"
+                    active
+                  />
+                : <Button
+                    type="primary"
+                    size="large"
+                    style={{
+                      float: direction === 'ltr'
+                        ? 'right'
+                        : 'left'
+                    }}
+                    danger={subIsActive}
+                    onClick={() => {
+                      onEdit({
+                        subId: data.sub.id,
+                        pause_collection: subIsActive
+                          ? { behavior: 'mark_uncollectible' }
+                          : ''
+                      })
+                    }}
+                    loading={data.loading}
+                  >
+                    {
+                      subIsActive
+                        ? 'Cancel'
+                        : 'Activate'
+                    }
+                  </Button>
+            }
+          </Col>
+        </Row>
+      </Card.Grid>
+    </Card>
+  )
+}
+
+export default MembershipSettings
