af07d66a6bd02f687a49832dab6d6d8aac8e7d38
adambarghouthi <adambargh@gmail.com>
2020-10-10 14:39:13 -0400

    add watchPmListMakeDefault, watchPmListMakeDefault

--- sagas/pmListSaga.js
+++ sagas/pmListSaga.js
@@ -4,11 +4,14 @@ import {
   actionTypes,
   handleSuccess,
   handleError,
+  handleFetch,
   handlePopulate
 } from 'actions/pmListActions'
 
+import { handleFetch as subFetch } from 'actions/subActions'
+
 function* pmListFetch(action) {
-  const { product } = action
+  const { cusId } = action
   const url = '/api/payment-methods'
 
   let token
@@ -16,16 +19,83 @@ function* pmListFetch(action) {
   try {
     token = localStorage.getItem('token')
 
+    const res = yield call(() => axios.get(url, {
+      params: {
+        token: token,
+        customerId: cusId
+      }
+    }))
+
     const { data } = res
     if (data.status === 'error') throw data
 
     const pms = data.data
 
-    yield put(handleSuccess())
+    yield put(handleSuccess(null))
     yield put(handlePopulate(pms))
   } catch (error) {
     yield put(handleError(error.message))
   }
+}
+
+function* pmListMakeDefault(action) {
+  const { pmId, subId } = action
+  const url = '/api/payment-methods/make-default'
+
+  let token
+
+  try {
+    token = localStorage.getItem('token')
+
+    const res = yield call(() => axios.put(url, {
+      token: token,
+      paymentMethodId: pmId
+    }))
+
+    const { data } = res
+    if (data.status === 'error') throw data
+
+    const pms = data.data
+
+    yield put(handleSuccess('make_default_pm_success'))
+  } catch (error) {
+    yield put(handleError(error.message))
+  }
+}
+
+function* pmListRemove(action) {
+  const { pmId } = action
+  const url = '/api/payment-methods'
+
+  let token
+
+  try {
+    token = localStorage.getItem('token')
+
+    const res = yield call(() => axios.delete(url, {
+      params: {
+        token: token,
+        paymentMethodId: pmId
+      }
+    }))
+
+    const { data } = res
+    if (data.status === 'error') throw data
+
+    const pm = data.data
+
+    yield put(handleSuccess('remove_pm_success'))
+  } catch (error) {
+    yield put(handleError(error.message))
+  }
+}
+
+export function* watchPmListRemove() {
+  yield takeLatest(actionTypes.PMLIST_REMOVE, pmListRemove)
+}
+
+export function* watchPmListMakeDefault() {
+  yield takeLatest(actionTypes.PMLIST_MAKE_DEFAULT, pmListMakeDefault)
 }
 
 export function* watchPmListFetch() {
