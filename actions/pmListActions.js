b6c3914972372dcb1755d9265686fd0c510946fd
adambarghouthi <adambargh@gmail.com>
2020-10-10 14:40:54 -0400

    add handleMakeDefault, handleRemove

--- actions/pmListActions.js
+++ actions/pmListActions.js
@@ -3,16 +3,29 @@ export const actionTypes = {
   PMLIST_SUCCESS: 'PMLIST_SUCCESS',
   PMLIST_ERROR: 'PMLIST_ERROR',
   PMLIST_POPULATE: 'PMLIST_POPULATE',
-  PMLIST_SUBMIT: 'PMLIST_SUBMIT'
+  PMLIST_SUBMIT: 'PMLIST_SUBMIT',
+  PMLIST_REMOVE: 'PMLIST_REMOVE',
+  PMLIST_MAKE_DEFAULT: 'PMLIST_MAKE_DEFAULT'
 }
 
-export const handlePopulate = mems => ({
+export const handlePopulate = pms => ({
   type: actionTypes.PMLIST_POPULATE,
   pms: pms
 })
 
-export const handleFetch = () => ({
-  type: actionTypes.PMLIST_FETCH
+export const handleMakeDefault = pmId => ({
+  type: actionTypes.PMLIST_MAKE_DEFAULT,
+  pmId: pmId
+})
+
+export const handleRemove = pmId => ({
+  type: actionTypes.PMLIST_REMOVE,
+  pmId: pmId
+})
+
+export const handleFetch = cusId => ({
+  type: actionTypes.PMLIST_FETCH,
+  cusId: cusId
 })
 
 export const handleSuccess = success => ({
