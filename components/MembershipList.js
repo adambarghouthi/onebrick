caccd093b972c7230b41df4b1066803bd5073e67
adambarghouthi <adambargh@gmail.com>
2020-10-13 15:12:34 -0400

    multiple changes
    detect selected, pass header prop

--- components/MembershipList.js
+++ components/MembershipList.js
@@ -9,65 +9,107 @@ const gridStyle = {
   width: '50%',
 }
 
-const prodId = process.env.NEXT_PUBLIC_PROD_ID
-
 const MembershipList = (props) => {
-  const { onFetch, onSelect, data } = props
-  const [fetched, setFetched] = useState(false)
+  const {
+    onSelect,
+    selected,
+    header,
+    data
+  } = props
+
   const { t } = useTranslation()
 
-  useEffect(() => {
-    if (!fetched) {
-      onFetch(prodId)
-      setFetched(true)
+  const isSelected = (index) => {
+    if (selected) {
+      if (data.mems.length) {
+        return data.mems[index].id === selected
+      }
     }
-  })
+
+    return false
+  }
 
   return (
     <Card>
       <Card.Grid hoverable={false} style={{ width: '100%' }}>
-        <Title level={4}>
+        <Title level={5}>
           { t('psf_membership') }
         </Title>
-        <Card.Meta
-          description={t('psf_membership_meta')}
-        />
-        <br />
-        <Space direction="vertical">
-          <Text><CheckCircleOutlined /> { t('cancel_or_edit') }</Text>
-          <Text><CheckCircleOutlined /> { t('view_activity') }</Text>
-          <Text><CheckCircleOutlined /> { t('receive_updates') }</Text>
-        </Space>
+        <p className="text-grey mb-0">
+          { t('psf_membership_meta') }
+        </p>
+        {
+          header === 'simple'
+            ? null
+            : <div className="mt-5">
+                <Space direction="vertical">
+                  <p className="mb-3">
+                    <CheckCircleOutlined />&nbsp;&nbsp;
+                    { t('cancel_or_edit') }
+                  </p>
+                  <p className="mb-3">
+                    <CheckCircleOutlined />&nbsp;&nbsp;
+                    { t('view_activity') }
+                  </p>
+                  <p className="mb-3">
+                    <CheckCircleOutlined />&nbsp;&nbsp;
+                    { t('receive_updates') }
+                  </p>
+                </Space>
+              </div>
+        }
       </Card.Grid>
       <Card.Grid style={gridStyle}>
         <Skeleton loading={data.loading} active>
-          <Title level={4}>{ `${t('support')} ${t('monthly')}` }</Title>
-          <Title className="my-0" level={2}>
-            ${data.mems.length ? data.mems[0].unit_amount / 100 : null}
+          <Text strong>
+            { `${t('support')} ${t('monthly')}` }
+          </Text>
+          <Title className="mt-5 mb-3" level={4}>
+            ${
+              data.mems.length
+                ? data.mems[0].unit_amount / 100
+                : null
+            }
           </Title>
           <Text type="secondary">{ t('per_month') }</Text>
           <Divider />
           <Button
             type="primary"
             onClick={() => onSelect(0)}
+            disabled={isSelected(0)}
           >
-            { t('select') }
+            {
+              isSelected(0)
+                ? t('selected')
+                : t('select')
+            }
           </Button>
         </Skeleton>
       </Card.Grid>
       <Card.Grid style={gridStyle}>
         <Skeleton loading={data.loading} active>
-          <Title level={4}>{ `${t('support')} ${t('yearly')}` }</Title>
-          <Title className="my-0" level={2}>
-            ${data.mems.length ? (data.mems[1].unit_amount / 100) : null}
+          <Text strong>
+            { `${t('support')} ${t('yearly')}` }
+          </Text>
+          <Title className="mt-5 mb-3" level={4}>
+            ${
+              data.mems.length
+                ? (data.mems[1].unit_amount / 100)
+                : null
+            }
           </Title>
           <Text type="secondary">{ t('per_year') }</Text>
           <Divider />
           <Button
             type="primary"
             onClick={() => onSelect(1)}
+            disabled={isSelected(1)}
           >
-            { t('select') }
+            {
+              isSelected(1)
+                ? t('selected')
+                : t('select')
+            }
           </Button>
         </Skeleton>
       </Card.Grid>
@@ -78,6 +120,10 @@ const MembershipList = (props) => {
       </Card.Grid>
     </Card>
   )
+}
+
+MembershipList.defaultProps = {
+  header: 'default',
 }
 
 export default MembershipList
