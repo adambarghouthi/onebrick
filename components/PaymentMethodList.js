314717d6151dee89a36b63191ec42b847af072aa
adambarghouthi <adambargh@gmail.com>
2020-10-13 15:16:59 -0400

    unfinished
    still needs to load when pms are being fetched

--- components/PaymentMethodList.js
+++ components/PaymentMethodList.js
@@ -1,68 +1,150 @@
 import React from 'react';
-import { List, Typography, Button, Dropdown, Menu, Tag } from 'antd';
-import { PlusOutlined, MoreOutlined } from '@ant-design/icons'
+import {
+  List,
+  Typography,
+  Button,
+  Dropdown,
+  Menu,
+  Tag,
+  Skeleton,
+  Modal
+} from 'antd';
+import {
+  PlusOutlined,
+  MoreOutlined,
+  ExclamationCircleOutlined
+} from '@ant-design/icons'
 import { LocaleContext } from 'context/LocaleContext'
 import { languageDirection } from 'lib/translations/config'
 import useTranslation from 'lib/translations/useTranslation'
 
-const { Title } = Typography
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-]
+const { Title, Text } = Typography
 
 const PaymentMethodList = (props) => {
+  const {
+    onFetch,
+    onAddNew,
+    onMakeDefault,
+    onRemove,
+    defaultPm,
+    data
+  } = props
+
   const { locale } = React.useContext(LocaleContext)
   const direction = languageDirection[locale] || 'ltr'
   const { t } = useTranslation()
+  const [modal, contextHolder] = Modal.useModal()
 
-  const { loading, fetchPaymentMethods } = props
-  const [showForm, setShowForm] = React.useState(false)
+  const confirmRemove = (pmId) => {
+    const pm = data.pms.filter(pm => pm.id === pmId)[0]
 
-  const overlay = (
-    <Menu onClick={() => {}}>
-      <Menu.Item key="1">
-        { t('set_default') }
-      </Menu.Item>
-      <Menu.Item key="2">
+    modal.confirm({
+      title: t('confirm_remove'),
+      icon: <ExclamationCircleOutlined />,
+      content: `**** ${pm.card.last4}`,
+      okText: t('yes'),
+      cancelText: t('no'),
+      maskClosable: true,
+      onOk: () => onRemove(pmId)
+    })
+  }
+
+  const overlay = (pmId, isDefault) => (
+    <Menu onClick={(item) => {
+      if (item.key === 'makeDefault') {
+        onMakeDefault(pmId)
+      } else if (item.key === 'remove') {
+        confirmRemove(pmId)
+      }
+    }}>
+      {
+        isDefault
+          ? null
+          : <Menu.Item key="makeDefault">
+              { t('make_default') }
+            </Menu.Item> 
+      }
+      <Menu.Item key="remove">
         { t('remove') }
       </Menu.Item>
     </Menu>
   )
 
   return (
-    <List
-      header={<Title className="mb-0" level={4}>{t('payment_method_list')}</Title>}
-      footer={<Button type="link"><PlusOutlined /> {t('add_new_payment_method')}</Button>}
-      bordered
-      dataSource={data}
-      renderItem={item => (
-        <List.Item
-          actions={[
-            <Dropdown
-              overlay={overlay}
-              placement={direction === 'ltr' ? 'bottomRight' : 'bottomLeft'}
+    <>
+      <List
+        bordered
+        header={
+          <Title
+            className="my-2"
+            level={5}>
+            { t('payment_methods') }
+          </Title>
+        }
+        footer={
+          <Button
+            type="link"
+            icon={<PlusOutlined />}
+            onClick={() => onAddNew()}>
+            { t('add_new_payment_method') }
+          </Button>
+        }
+        dataSource={data.pms}
+        locale={{ emptyText: t('no_data') }}
+        size="large"
+        renderItem={(pm, index) => (
+          <List.Item
+            actions={[
+              <Dropdown
+                overlay={overlay(pm.id, pm.id === defaultPm)}
+                placement={direction === 'ltr'
+                  ? 'bottomRight'
+                  : 'bottomLeft'
+                }
+              >
+                <Button icon={<MoreOutlined />} />
+              </Dropdown>
+            ]}
+          >
+            <Skeleton
+              loading={data.loading}
+              paragraph={{ rows: 0 }}
+              avatar={{ shape: 'square' }}
+              active
             >
-              <Button>
-                <MoreOutlined />
-              </Button>
-            </Dropdown>
-          ]}
-        >
-          <List.Item.Meta
-            avatar={
-              <img src="/assets/visa.png" height="45px" />
-            }
-            title={<div>Visa **** 1234&nbsp;&nbsp;&nbsp;<Tag>{t('default')}</Tag></div>}
-            description={`${t('expiration_date')}: 20/12/2020`}
-          />
-        </List.Item>
-      )}
-    />
+              <List.Item.Meta
+                avatar={
+                  <img src={`/assets/${pm.card.brand}.png`} height="45px" />
+                }
+                title={
+                  <div>
+                    <Text>
+                      {
+                        pm.card.brand.charAt(0).toUpperCase()
+                        + pm.card.brand.slice(1)
+                      }&nbsp;
+                    </Text>
+                    <Text>
+                      **** { pm.card.last4 }
+                      &nbsp;&nbsp;&nbsp;
+                    </Text>
+                    {
+                      pm.id === defaultPm
+                        ? <Tag>{ t('default') }</Tag>
+                        : null
+                    }
+                  </div>
+                }
+                description={
+                  `${t('expiration_date')}: ${('0' + pm.card.exp_month).slice(-2)}/${pm.card.exp_year}`
+                }
+              />
+            </Skeleton>
+          </List.Item>
+        )}
+      />
+      { contextHolder }
+    </>
   )
 }
 
