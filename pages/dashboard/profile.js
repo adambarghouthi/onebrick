f232bfe49b95141f47d642589b9b47f30b92cf15
adambarghouthi <adambargh@gmail.com>
2020-12-06 12:11:54 -0500

    mv into pages folder

---
+++ pages/dashboard/profile.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useContext } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { compose } from 'redux'
+import Head from 'next/head'
+import { DashboardLayout } from 'components/layouts'
+import { ChangePwdForm, ProfileForm } from 'components/forms'
+import { LocaleContext } from 'context/LocaleContext'
+import { languageDirection } from 'lib/translations/config'
+import useTranslation from 'lib/translations/useTranslation'
+import handleMessage from 'lib/handleMessage'
+import withLocale from 'hocs/withLocale'
+import withAuth from 'hocs/withAuth'
+
+import {
+  handleChange as profileFormOnChange,
+  handleSubmit as profileFormOnSubmit
+} from 'actions/profileFormActions'
+
+import {
+  handleChange as changePwdFormOnChange,
+  handleSubmit as changePwdFormOnSubmit
+} from 'actions/changePwdFormActions'
+
+const Profile = () => {
+  const dispatch = useDispatch()
+  const profileForm = useSelector(state => state.profileForm)
+  const changePwdForm = useSelector(state => state.changePwdForm)
+  const user = useSelector(state => state.user)
+
+  const { t } = useTranslation()
+  const { locale } = useContext(LocaleContext)
+  const direction = languageDirection[locale] || 'ltr'
+
+  // populate profileForm once
+  useEffect(() => {
+    dispatch(profileFormOnChange(null, {
+      ...profileForm,
+      email: user.email
+    }))
+  }, [user.email])
+
+  // track profileForm success & error
+  useEffect(() => {
+    const { success, error } = profileForm
+    handleMessage(success, error, t)
+  }, [profileForm.success, profileForm.error])
+
+  // track changePwdForm success & error
+  useEffect(() => {
+    const { success, error } = changePwdForm
+    handleMessage(success, error, t)
+  }, [changePwdForm.success, changePwdForm.error])
+
+  return (
+    <>
+      <Head>
+        <title>{ t('profile_hd_title') }</title>
+      </Head>
+      <DashboardLayout>
+        <ProfileForm
+          onChange={(a, b) => dispatch(profileFormOnChange(a, b))}
+          onSubmit={form => dispatch(profileFormOnSubmit(form))}
+          data={profileForm}
+        />
+        <br />
+        <ChangePwdForm
+          onChange={(a, b) => dispatch(changePwdFormOnChange(a, b))}
+          onSubmit={form => dispatch(changePwdFormOnSubmit(form))}
+          data={changePwdForm}
+        />
+      </DashboardLayout>
+    </>
+  )
+}
+
+export default compose(withLocale, withAuth)(Profile)
---
+++ pages/login.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react'
+import { compose } from 'redux'
+import { useDispatch, useSelector } from 'react-redux'
+import Head from 'next/head'
+import {
+  message,
+  Card,
+  Form,
+  Input,
+  Button,
+  Layout,
+  Space,
+  Row,
+  Col
+} from 'antd'
+import { GeneralLayout } from 'components/layouts'
+import { LoginForm } from 'components/forms'
+import useTranslation from 'lib/translations/useTranslation'
+import handleMessage from 'lib/handleMessage'
+import withLocale from 'hocs/withLocale'
+import withNonAuth from 'hocs/withNonAuth'
+
+import {
+  handleChange,
+  handleSubmit
+} from 'actions/loginFormActions'
+
+const Login = () => {
+  const dispatch = useDispatch()
+  const loginForm = useSelector(state => state.loginForm)
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    const { success, error } = loginForm
+    handleMessage(success, error, t)
+  }, [loginForm.success, loginForm.error])
+
+  return (
+    <div>
+      <Head>
+        <title>{t('login_title')}</title>
+      </Head>
+      <GeneralLayout>
+        <Layout className="container">
+          <Row type="flex" justify="center">
+            <Col xs={24} sm={16}>
+              <LoginForm
+                onChange={(a, b) => dispatch(handleChange(a, b))}
+                onSubmit={form => dispatch(handleSubmit(form))}
+                data={loginForm}
+              />
+            </Col>
+          </Row>
+        </Layout>
+      </GeneralLayout>
+    </div>
+  )
+}
+
+export default compose(withLocale, withNonAuth)(Login)
