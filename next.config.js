8bef8f0c10035675443d5466b7a81246cdb72c14
adambarghouthi <adambargh@gmail.com>
2020-12-06 12:06:58 -0500

    add localization config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer()
+module.exports = (phase, { defaultConfig }) => {
+  const config = {
+    ...defaultConfig,
+    i18n: {
+      locales: ['en', 'ar'],
+      defaultLocale: 'en',
+    },
+  };
+
+  return withBundleAnalyzer(config);
+};
