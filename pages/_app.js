2085e2a86208a1ae69da120d854dbc2106b3f854
adambarghouthi <adambargh@gmail.com>
2020-12-06 13:01:37 -0500

    minor

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import 'styles/vars.css';
 import 'styles/global.css';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 };
 
 export default wrapper.withRedux(App);
