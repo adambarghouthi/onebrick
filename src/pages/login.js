23111d9237ce5a41fdcdc3629b6755bd30564802
adambarghouthi <adambargh@gmail.com>
2020-12-15 18:33:49 -0500

    minor

--- src/pages/login.js
+++ src/pages/login.js
@@ -37,7 +37,7 @@ const Login = () => {
   return (
     <div>
       <Head>
-        <title>{t('login_title')}</title>
+        <title>{t('login_hd_title')}</title>
       </Head>
       <GeneralLayout>
         <Layout className="container">
